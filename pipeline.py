76c016b25be5fbfacfd373e2b3d8d6732689aadf
Alexander Aeschbacher <narwal27@protonmail.com>
2025-12-03 16:54:10 +0100

    remove fof

--- pipeline.py
+++ pipeline.py
@@ -65,7 +65,7 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
                           'coordinates_feature': True,
                           'intensity_feature': True,
                           'gradient_intensity_feature': True,
-                          'first_order_feature': True}
+                          'first_order_feature': False}
 
     # load images for training and pre-process
     images = putil.pre_process_batch(crawler.data, pre_process_params, multi_process=False)
