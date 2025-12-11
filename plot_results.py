da3eea1ed88b0dc9a58c44cf81c3cf2c7db46ce9
Alexander Aeschbacher <narwal27@protonmail.com>
2025-12-11 18:20:02 +0100

    add plotting

--- plot_results.py
+++ plot_results.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import glob, os
 
 
 def main():
@@ -10,7 +12,29 @@ def main():
 
     # alternative: instead of manually loading/reading the csv file you could also use the pandas package
     # but you will need to install it first ('pip install pandas') and import it to this file ('import pandas as pd')
-    pass  # pass is just a placeholder if there is no other code
+    df = pd.read_csv('mia-result/2025-12-05-13-39-10/results.csv', sep=";")
+    
+    amy = df[df['LABEL'] == 'WhiteMatter']
+ 
+    csv_files = glob.glob('mia-result/*/results_summary.csv', recursive=True)
+
+    all_data = []
+
+    for file_path in csv_files:
+        # Extract directory name (date) - parent directory of the file
+        date = os.path.basename(os.path.dirname(file_path))
+        
+        # Read CSV
+        df = pd.read_csv(file_path, sep=";")
+        
+        # Add date column
+        df['date'] = date
+        
+        all_data.append(df)
+
+    # Combine all DataFrames
+    combined_df = pd.concat(all_data, ignore_index=True)
+    print(combined_df)
 
 
 if __name__ == '__main__':
